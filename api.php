5e652af9aeed157a7b5fa08c954b65e833dd4c07
Paolocargnin <cargnin.paolo@gmail.com>
2014-11-19 17:03:22 +0100

    something

---
+++ api.php
@@ -0,0 +1,13 @@
+<?php
+$data = file_get_contents("php://input");
+
+$objData = json_decode($data);
+
+if ($objData->action == 'register'){
+	$users=unserialize(file_get_contents('db.txt'));
+	$users[]=array("name"=>$objData->name,"mail"=>$objData->mail);
+	print(serialize($users));
+	file_put_contents( 'db.txt' , serialize($users) );
+	echo "User inserito";
+}
+?>
